37428bb91223d61650a0df39861e8e01937edb0a
Kamal <58348770+leavekamalone@users.noreply.github.com>
2021-01-21 23:43:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://kamal416:ripkobe@cluster0.cuzan.mongodb.net/inventory?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://USERNAME:PASS@cluster0.cuzan.mongodb.net/inventory?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
